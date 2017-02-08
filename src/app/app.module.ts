a271341f61b2986a97f41d1eddba4001cde8c2d2
Danial Taylor <danial@graphventure.com>
2017-02-08 00:48:50 -0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    EventsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
